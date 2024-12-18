32f4285ead38670f321c9494f90d3694d6a5c55f
Andrew <kezin636@gmail.com>
2024-12-18 18:19:04 +0300

    dangerous!

--- oneline.sh
+++ oneline.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 
+if [ ! -d "essential-scripts" ]; then
+  git clone https://github.com/GMDProjectL/essential-scripts
+  cd essential-scripts
+else
+  cd essential-scripts
+  git pull
+fi
+
+./install_essentials.sh
